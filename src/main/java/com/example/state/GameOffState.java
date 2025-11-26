77a7fd9b17958c40e4929e22d29f4fdd88f655dc
Critk <davidchiemeke10@gmail.com>
2025-11-26 16:20:34 -0400

    Commands Commit

--- src/main/java/com/example/state/GameOffState.java
+++ src/main/java/com/example/state/GameOffState.java
@@ -1,5 +1,33 @@
 package com.example.state;
+import com.example.core.Game;
 
-public class GameOffState {
+public class GameOffState implements IGameState{
+
+    @Override
+    public void start(Game game) { 
+        game.setState(new GameOnState()); 
+        game.getLogger().logSystem("Start Game"); 
+    }
+
     
+    @Override
+    public void end(Game game) { 
+        //already off
+    
+    }
+
+
+    @Override
+    public void processSelection(Game game, String category, int value) { 
+         //not expected here
+    
+    }
+
+
+    @Override
+    public void processAnswer(Game game, String answer) { 
+        //not expected here
+    
+    }
 }
+
