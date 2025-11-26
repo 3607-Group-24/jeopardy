3564dbcd84468fa1cba960b844a0df69579dce67
Critk <davidchiemeke10@gmail.com>
2025-11-26 18:57:41 -0400

    New Test Commits

--- src/test/java/com/example/scoring/ScoringTest.java
+++ src/test/java/com/example/scoring/ScoringTest.java
@@ -1,5 +1,18 @@
-package scoring;
+package com.example.scoringstrategy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class ScoringTest {
-    
+    @Test
+    public void correctAdds() {
+        CorrectAnswer s = new CorrectAnswer();
+        assertEquals(100, s.calculateScore(100));
+    }
+    @Test
+    public void wrongSubtracts() {
+        WrongAnswer s = new WrongAnswer();
+        assertEquals(-50, s.calculateScore(50));
+    }
 }
+
