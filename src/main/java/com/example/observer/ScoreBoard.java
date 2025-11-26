cfe9ce464a6084c3969122d2da3b8cdc636cf018
RonnieM03 <sherandmaitland@gmail.com>
2025-11-26 17:51:19 -0400

    commit for observer and utilities

--- src/main/java/com/example/observer/ScoreBoard.java
+++ src/main/java/com/example/observer/ScoreBoard.java
@@ -1,5 +1,26 @@
 package com.example.observer;
 
-public class ScoreBoard {
+import java.util.List;
+
+import com.example.model.GameEvent;
+import com.example.model.Player;
+
+public class ScoreBoard implements IGameObserver {
+
+    private final List<Player> players;
+
+    public ScoreBoard(List<Player> players) { this.players = players; }
+
+
+    @Override
+    public void update(GameEvent event) {
+        System.out.println("[EVENT] " + event.getActivity() + " | " + event.getPlayerId());
+    }
+
+    public void show() {
+        System.out.println("----- SCOREBOARD -----");
+        for (Player p : players) System.out.println(p.getId() + ": $" + p.getScore());
+        System.out.println("----------------------");
+    }
     
 }
